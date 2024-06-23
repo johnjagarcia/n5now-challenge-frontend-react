fe3101cf804f12ee429359aa9182c192f5a0f2de
John Jairo Garcia Castro <johnjagarcia94@gmail.com>
2024-06-22 23:20:33 -0500

    Fix naming files

---
+++ src/routes/CreateProduct.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateProductForm from "../components/product/CreateProductForm";
+
+const CreateProduct = () => {
+  return (
+    <div>
+      <CreateProductForm />
+    </div>
+  );
+};
+
+export default CreateProduct;
